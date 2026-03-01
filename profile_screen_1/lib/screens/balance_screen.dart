e111c9b602e3420a82c2a383bcb842d02b35a5fd
Max <slamax838@gmail.com>
2026-03-01 23:08:06 +0100

    little fix 2

--- profile_screen_1/lib/screens/balance_screen.dart
+++ profile_screen_1/lib/screens/balance_screen.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class BalanceScreen extends StatelessWidget {
+  const BalanceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: const Text('Balance screen'));
+  }
+}
